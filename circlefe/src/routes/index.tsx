30cabedc05250695919dd0219424a4e07af68cc9
unknown <satrianieros33@gmail.com>
2024-09-26 18:13:00 +0700

    try again and again

--- circlefe/src/routes/index.tsx
+++ circlefe/src/routes/index.tsx
@@ -14,7 +14,7 @@ export function AppRouter() {
     },
     {
       path: "/login",
-      element: <LoginRoute />,
+      element: <HomeRoute />,
     },
     {
       element: <ProtectedRoutes />,
